98afd74e0369558682f14b64fb65bdcb90fb8e4e
MineRaphi <MineRaphiHD@gmail.com>
2026-06-16 09:23:04 +0200

    Configured rehype-external-links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank' }]
+    ]
+  }
 });
